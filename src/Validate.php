67efc111f0c3c3fceb341fc5ff4dc8d452a88b95
Albert <avalls89@gmail.com>
2022-02-24 16:43:59 +0100

    23/02/22

--- src/Validate.php
+++ src/Validate.php
@@ -8,4 +8,16 @@ class Validate
     {
         return (bool) filter_var($value, FILTER_VALIDATE_EMAIL);
     }
+
+    public static function url($value)
+    {
+        return (bool) filter_var($value, FILTER_VALIDATE_URL);
+    }
+
+    public static function pwd($value)
+    {
+        $regex = 
+        return (bool) filter_var($value, FILTER_VALIDATE_REGEXP);
+    }
+}
 }
--- tests/ValidateTest.php
+++ tests/ValidateTest.php
@@ -9,5 +9,17 @@ class ValidateTest extends TestCase
     {
         $email = Validate::email('als89@gmail.com');
         $this->assertTrue($email);
+
+        $email = Validate::email('als89@@gmail.com');
+        $this->assertFalse($email);
+    }
+
+    public function test_url()
+    {
+        $url = Validate::url('https://platzi.com');
+        $this->assertTrue($url);
+
+        $url = Validate::url('platzi.com');
+        $this->assertFalse($url);
     }
 }
